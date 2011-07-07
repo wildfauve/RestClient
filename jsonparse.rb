e8e30f17c114b3847d08524988ee73ad07fc91bd
Wild Fauve <wild.fauve@gmail.com>
2011-07-07 19:43:53 +1200

    Update

--- jsonparse.rb
+++ jsonparse.rb
@@ -1,5 +1,6 @@
 require 'rest-client'
+require 'json'
 
 resource = RestClient::Resource.new 'http://localhost:3000/trees'
-main = resource.get(:accept => 'application/json')
+main = JSON.parse(resource.get(:accept => 'application/json'))
 puts main.inspect
