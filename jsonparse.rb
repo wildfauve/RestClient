5c960bfd085eba259e7e4c319cc604e414bbcf28
Wild Fauve <wild.fauve@gmail.com>
2011-08-02 09:47:09 +1200

    Revert e8e30f17c114b3847d08524988ee73ad07fc91bd^..HEAD

--- jsonparse.rb
+++ jsonparse.rb
@@ -1,6 +1,5 @@
 require 'rest-client'
-require 'json'
 
 resource = RestClient::Resource.new 'http://localhost:3000/trees'
-main = JSON.parse(resource.get(:accept => 'application/json'))
+main = resource.get(:accept => 'application/json')
 puts main.inspect
